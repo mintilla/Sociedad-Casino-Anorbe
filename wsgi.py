c79605c77c2a0fcfff181721342989184405414d
Mikel Intillaque <mintilla@MacBook-Air.local>
2025-03-09 23:43:15 +0100

    Corregir configuración para Render

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,4 @@
-from app.py import app  # Reemplaza "tu_aplicacion" con el nombre de tu archivo principal
+from app import app  # Reemplaza "tu_aplicacion" con el nombre de tu archivo principal
 
 if __name__ == "__main__":
     app.run()
